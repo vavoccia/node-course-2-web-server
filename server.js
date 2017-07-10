e3a8e55717015879d5a5c24748872eadd7b1a926
Vince Voccia <vvoccia@outlook.com>
2017-07-10 13:39:10 -0400

    added projects.hbs, updated header.hbs

--- server.js
+++ server.js
@@ -42,6 +42,11 @@ app.get('/about', (req, res) => {
         pageTitle: 'About Page',
     });
 });
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page',
+    });
+});
 app.listen(port,()=>{
     console.log(`Server is up and Running on port: ${port}`);
 });
